d354f01ef8ab29d7454b3276ebb3bc1439ad5446
AIE\s220552 <austinlanyon@gmail.com>
2024-05-20 11:28:39 +1000

    Camera moving is now fully implemented

--- TopDownGame/Assets/Scripts/CameraMove.cs
+++ TopDownGame/Assets/Scripts/CameraMove.cs
@@ -8,20 +8,50 @@ public class CameraMove : MonoBehaviour
 	public Transform playPosition = null; // create an empty gameobject and assign in inspector
 	public Transform settingPosition = null; // create an empty gameobject and assign in inspector
 	public GameObject mainMenu;
-	public GameObject settings;
+    public GameObject playMenu;
+    public GameObject settings;
 
-	public MainMenu mainMenuScript;
-	void Update()
+	public MenuUI mainMenuScript;
+
+    private void Start()
+    {
+        mainMenuScript = GameObject.Find("Main_Menu").GetComponent<MenuUI>();
+        mainMenu.SetActive(true);
+        playMenu.SetActive(false);
+        settings.SetActive(false);
+    }
+    void Update()
 	{
-		mainMenuScript = GameObject.Find("Main_Menu").GetComponent<MainMenu>();
 		float camPos = mainMenuScript.camPos;
 
-		if (camPos == 1)
+		
+        if (camPos == 1)
         {
 			transform.position = Vector3.Lerp(transform.position, playPosition.position, Time.deltaTime);
 			float rotateX = 90;
 			transform.eulerAngles = new Vector3(rotateX, 0, 0);
+			mainMenu.SetActive(false);
+			playMenu.SetActive(true);
+            settings.SetActive(false);
         }
-		
-	}
+        else if (camPos == 2)
+        {
+            transform.position = Vector3.Lerp(transform.position, settingPosition.position, Time.deltaTime);
+            float rotateY = 106;
+            transform.eulerAngles = new Vector3(0, rotateY, 0);
+            mainMenu.SetActive(false);
+            playMenu.SetActive(false);
+            settings.SetActive(true);
+        }
+        if (camPos == 3)
+        {
+            transform.position = Vector3.Lerp(transform.position, mainMenuPosition.position, Time.deltaTime);
+            float rotateY = -20;
+            transform.eulerAngles = new Vector3(0, rotateY, 0);
+            mainMenu.SetActive(true);
+            playMenu.SetActive(false);
+            settings.SetActive(false);
+        }
+
+    }
 }
--- TopDownGame/Assets/Scripts/MenuUI.cs
+++ TopDownGame/Assets/Scripts/MenuUI.cs
@@ -3,12 +3,13 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class MainMenu : MonoBehaviour
+public class MenuUI : MonoBehaviour
 {
-    public float camPos = 1; //0 = main menu position, 1 = play position, 2= setting position
+    public float camPos = 1; //3 = main menu position, 1 = play position, 2= setting position
     
     public void ClickButton(int buttonClicked)
     {
+       
         if (buttonClicked == 1)
         {
             camPos = 1;
@@ -16,6 +17,10 @@ public class MainMenu : MonoBehaviour
         else if (buttonClicked == 2)
         {
             camPos = 2;
+        }
+        else if (buttonClicked == 3)
+        {
+            camPos = 3;
         }
     }
 }
