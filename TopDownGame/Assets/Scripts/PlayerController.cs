55943aa555e83770b69f5092c2819a70383cc2e2
AIE\s220552 <austinlanyon@gmail.com>
2024-05-27 10:44:44 +1000

    Aiming with mouse or joystick is now in

--- TopDownGame/Assets/Scripts/PlayerController.cs
+++ TopDownGame/Assets/Scripts/PlayerController.cs
@@ -3,32 +3,97 @@ using System.Collections.Generic;
 using UnityEditor;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using static UnityEditor.FilePathAttribute;
 
 public class PlayerController : MonoBehaviour
 {
     public float speed;
-    private Vector2 move;
+    private Vector2 move, mouseLook, joystickLook;
+    private Vector3 rotationTarget;
+    public bool isPC;
 
     public void OnMove(InputAction.CallbackContext context)
     {
         move = context.ReadValue<Vector2>();
     }
-    // Start is called before the first frame update
+
+    public void OnMouseLook(InputAction.CallbackContext context)
+    {
+        mouseLook = context.ReadValue<Vector2>();
+    }
+
+    public void OnJoystickLook(InputAction.CallbackContext context)
+    {
+        joystickLook = context.ReadValue<Vector2>();
+    }
+
     void Start()
     {
         
     }
-
-    // Update is called once per frame
+       
     void Update()
     {
-        movePlayer();
+        if(isPC)
+        {
+            RaycastHit hit;
+            Ray ray = Camera.main.ScreenPointToRay(mouseLook);
+
+            if(Physics.Raycast(ray, out hit))
+            {
+                rotationTarget = hit.point;
+            }
+            movePlayerWithAim();
+        }
+        else
+        {
+            if(joystickLook.x == 0 && joystickLook.y == 0)
+            {
+                movePlayer();
+            }
+            else
+            {
+                movePlayerWithAim();
+            }
+        }
     }
 
     public void movePlayer()
     {
         Vector3 movement = new Vector3(move.x, 0, move.y);
-        transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(movement), 0.15f);
+        if(movement != Vector3.zero)
+        {
+            transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(movement), 0.15f);
+        }
+        transform.Translate(movement * speed * Time.deltaTime, Space.World);
+    }
+
+    public void movePlayerWithAim()
+    {
+        if (isPC)
+        {
+            var lookPos = rotationTarget - transform.position;
+                lookPos.y = 0;
+            var rotation = Quaternion.LookRotation(lookPos);
+
+            Vector3 aimDirection = new Vector3(rotationTarget.x, 0f, rotationTarget.z);
+
+            if(aimDirection != Vector3.zero)
+            {
+                transform.rotation = Quaternion.Slerp(transform.rotation, rotation, 0.15f);
+            }
+        }
+        else
+        {
+            Vector3 aimDirection = new Vector3(joystickLook.x, 0, joystickLook.y);
+
+            if (aimDirection != Vector3.zero)
+            {
+                transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(aimDirection), 0.15f);
+            }
+        }
+
+        Vector3 movement = new Vector3(move.x, 0f, move.y);
         transform.Translate(movement * speed * Time.deltaTime, Space.World);
     }
 }
