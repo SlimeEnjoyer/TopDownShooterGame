998305b8c37fd59c7fd1d3325a79b772dad069f1
AIE\s220552 <austinlanyon@gmail.com>
2024-05-28 12:07:20 +1000

    players can look if different directions now

--- TopDownGame/Assets/Scripts/PlayerController.cs
+++ TopDownGame/Assets/Scripts/PlayerController.cs
@@ -6,15 +6,8 @@ public class PlayerController : MonoBehaviour
 {
     [SerializeField]
     private float playerSpeed = 2.0f;
-    [SerializeField]
-    private float gravityValue = -9.81f;
-
     private CharacterController controller;
-    private Vector3 playerVelocity;
-    private bool groundedPlayer;
-
-    private Vector2 movementInput = Vector2.zero;
-
+    private Vector2 movementInput = Vector2.zero, lookInput;
 
     private void Start()
     {
@@ -26,26 +19,40 @@ public class PlayerController : MonoBehaviour
         movementInput = context.ReadValue<Vector2>();
     }
 
+    public void OnLook(InputAction.CallbackContext context)
+    {
+        lookInput = context.ReadValue<Vector2>();
+        //Debug.Log("lookInput = " + lookInput);
+    }
+
     void Update()
     {
-        groundedPlayer = controller.isGrounded;
-        if (groundedPlayer && playerVelocity.y < 0)
-        {
-            playerVelocity.y = 0f;
-        }
 
+            movePlayer();
+ 
+            moveWithAim();
+    }
+
+    public void movePlayer()
+    {
         Vector3 move = new Vector3(movementInput.x, 0, movementInput.y);
+        //Debug.Log("Input MOVE values = " + move);
         controller.Move(move * Time.deltaTime * playerSpeed);
-
-        // looking around
         //if (move != Vector3.zero)
         //{
-        //    gameObject.transform.forward = move;
+        //    transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(move), 0.15f);
         //}
+    }
 
-        // Changes the height position of the player..
-
-        playerVelocity.y += gravityValue * Time.deltaTime;
-        controller.Move(playerVelocity * Time.deltaTime);
+    public void moveWithAim()
+    {
+        Vector3 aimDirection = new Vector3(lookInput.x, 0f, lookInput.y);
+        //Debug.Log("Input ROTATE values = " + aimDirection);
+        if (aimDirection != Vector3.zero)
+        {
+            transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(aimDirection), 0.15f);
+        }
+        //Vector3 movement = new Vector3(movementInput.x, 0f, movementInput.y);
+        //transform.Translate(movement * playerSpeed * Time.deltaTime, Space.World);
     }
 }
